921b7f2662e0b9ec0fbe840f0fb618c406ecc2d5
hernanicoelho <coelhohernani09@gmail.com>
2020-04-13 22:25:32 +0000

    Menu completo

--- ui/menu.c
+++ ui/menu.c
@@ -3,39 +3,226 @@
 #include <string.h>
 #include "menu.h"
 
-//Menu com lista de opções possíveis
-void menuPrincipal()
-{
+//Menu principal com as opções possíveis
+void menuPrincipal() {
     int opcao;
-    do
-    {
+    
+    do {
         wprintf(L"Menu Principal\n");
         wprintf(L"Escolha uma das seguintes opções:\n");
-        wprintf(L"1-Incrições\n");
-        wprintf(L"2-Anular incrições\n");
-        wprintf(L"3-Listagem de inscrições\n");
-        wprintf(L"4-Calcular propinas\n");
-        wprintf(L"5-Gravar\n");
-        wprintf(L"0-Sair\n");
-        wprintf(L"Opção escolhida: ");
-        wscanf(L"%d\n", &opcao);
+        wprintf(L"1 - Alunos\n");
+        wprintf(L"2 - UC's\n");
+        wprintf(L"3 - Incrições\n");
+        wprintf(L"4 - Propinas\n");
+        wprintf(L"5 - Consultas\n");
+        wprintf(L"6 - Reports\n");
+        wprintf(L"0 - Sair\n");
 
-        switch (opcao)
-        {
+        wscanf(L"%d", &opcao);
+
+        switch (opcao) {
             case 1:
-                inscricaoAluno(); //Incricão aluno numa UC
+                menuAlunos(); 
                 break;
             case 2:
-                anularInscricao(); //Anular inscrição aluno numa UC
+                menuUCS(); 
                 break;
             case 3:
-                listaInscricao(); //Listagem de inscrições
+                menuInscricoes(); 
                 break;
             case 4:
-                calculoPropinas(); //Valor de propinas
+                menuPropinas(); 
                 break;
             case 5:
-                gravarDados(); //Gravar dados antes de sair
+                menuConsultas();
+            case 6:
+                menuReports(); 
+            case 0:
+                break;
+            default:
+                wprintf(L"Opção inválida!\n");
+        }
+    } while (opcao != 0);
+}
+
+//Submenu para funções relacionadas directamente com alunos
+void menuAlunos() {
+    int opcao;
+
+    do {
+        wprintf(L"1 - Adicionar Aluno\n");
+        wprintf(L"2 - Remover Aluno\n");
+        wprintf(L"3 - Consultar Aluno\n");
+        wprintf(L"4 - Modificar Aluno\n");
+        wprintf(L"0 - Menu anterior\n");
+
+        wscanf(L"%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                adicionarAluno();
+                break;
+            case 2:
+                removerAluno();
+                break;
+            case 3:
+                consultarAluno();
+                break;
+            case 4:
+                modificarAluno();
+                break;
+            case 0:
+                break;
+            default:
+                wprintf(L"Opção inválida!\n");
+        }
+    } while (opcao != 0);
+}
+
+//Submenu para funções relacionadas directamente com UCs
+void menuUCS() {
+    int opcao;
+
+    do {
+        wprintf(L"1 - Adicionar UC\n");
+        wprintf(L"2 - Remover UC\n");
+        wprintf(L"3 - Consultar UC\n");
+        wprintf(L"4 - Modificar UC\n");
+        wprintf(L"0 - Menu anterior\n");
+
+        wscanf(L"%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                adicionarUC();
+                break;
+            case 2:
+                removerUC();
+                break;
+            case 3:
+                consultarUC();
+                break;
+            case 4:
+                modificarUC();
+                break;
+            case 0:
+                break;
+            default:
+                wprintf(L"Opção inválida!\n");
+        }
+    } while (opcao != 0);
+}
+
+//Submenu para funções relacionadas directamente com incrições
+void menuInscricoes() {
+    int opcao;
+
+    do {
+        wprintf(L"1 - Adicionar Inscrição\n");
+        wprintf(L"2 - Remover Inscrição\n");
+        wprintf(L"3 - Consultar Inscrição\n");
+        wprintf(L"4 - Modificar Inscrição\n");
+        wprintf(L"0 - Menu anterior\n");
+
+        wscanf(L"%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                adicionarInscricao();
+                break;
+            case 2:
+                removerInscricao();
+                break;
+            case 3:
+                consultarInscricao();
+                break;
+            case 4:
+                modificarInscricao();
+                break;
+            case 0:
+                break;
+            default:
+                wprintf(L"Opção inválida!\n");
+        }
+    } while (opcao != 0);
+}
+
+
+//Submenu para funções relacionadas directamente com propinas
+void menuPropinas() {
+    int opcao;
+
+    do {
+        wprintf(L"1 - Calcular Propinas\n");
+        wprintf(L"0 - Menu anterior\n");
+
+        wscanf(L"%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                calcularPropina();
+                break;
+            case 0:
+                break;
+            default:
+                wprintf(L"Opção inválida!\n");
+        }
+    } while (opcao != 0);
+}
+
+
+//Submenu para funções relacionadas directamente com consultas
+void menuConsultas() {
+    int opcao;
+
+    do {
+        wprintf(L"1 - Lista de inscrições por aluno\n");
+        wprintf(L"2 - Lista de inscrições por UC\n");
+        wprintf(L"0 - Menu anterior\n");
+
+        wscanf(L"%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                listaInscAluno();
+                break;
+            case 2:
+                listaInscUC();
+                break;
+            case 0:
+                break;
+            default:
+                wprintf(L"Opção inválida!\n");
+        }
+    } while (opcao != 0);
+}
+
+//Submenu para funções relacionadas directamente com reports
+void menuReports() {
+    int opcao;
+
+    do {
+        wprintf(L"1 - Número total de ECTS por aluno\n");
+        wprintf(L"2 - Alunos para época especial de exames\n");
+        wprintf(L"3 - Situações de potencial abandono escolar\n");
+        wprintf(L"4 - Número de alunos por ano letivo\n");
+        wprintf(L"0 - Menu anterior\n");
+
+        wscanf(L"%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                repECTSAluno();
+                break;
+            case 2:
+                repEpocaEspExame();
+                break;
+            case 3:
+                repAbanEscolar();
+                break;
+            case 4:
+                repTotalAlunAnoLet();
+                break;
             case 0:
                 break;
             default:
--- ui/menu.h
+++ ui/menu.h
@@ -1,2 +1,14 @@
 //Menu com lista de opções possíveis
 void menuPrincipal();
+//Submenu para funções relacionadas directamente com alunos
+void menuAlunos();
+//Submenu para funções relacionadas directamente com UCs
+void menuUCS();
+//Submenu para funções relacionadas directamente com incrições
+void menuInscricoes();
+//Submenu para funções relacionadas directamente com propinas
+void menuPropinas();
+//Submenu para funções relacionadas directamente com consultas
+void menuConsultas();
+//Submenu para funções relacionadas directamente com reports
+void menuReports();
