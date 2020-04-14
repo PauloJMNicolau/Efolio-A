036da4544bc11a521f4986ae7b2aa7d79517412f
Paulo Nicolau <paulojmnicolau227@gmail.com>
2020-04-14 21:17:25 +0100

    reconfiguração arvore

--- sgbd/sgbd.c
+++ sgbd/sgbd.c
@@ -5,6 +5,7 @@
 #include "sgbd.h"
 #include "macro.h"
 
+
 /************************************
  *          Motor SGBD              *
  ************************************/
@@ -29,60 +30,6 @@ SGBD *criarBaseDados(){
 
 
 
-
-/*
-//Remover elementos da arvore
-int removerElementoInscricao(NoINSCRICAO * inscricao, TREE_INSCRICAO * arvore){
-    if(!arvore){
-        return _ERR_EMPTYLIST;
-    }
-    if(inscricao == arvore->raiz) //Remover na raiz da arvore
-        arvore->raiz = removerElementoNaSubArvore(inscricao,arvore->raiz);
-    else{
-        int res = wcscmp(inscricao->elemento->anoLetivo, arvore->raiz->elemento->anoLetivo);
-        if(res ==0)
-            arvore->raiz->ramo = removerElementoNaSubArvore(inscricao,arvore->raiz->ramo);
-        else if(res < 0)
-            arvore->raiz->anterior = removerElementoNaSubArvore(inscricao, arvore->raiz->anterior);
-        else
-            arvore->raiz->proximo = removerElementoNaSubArvore(inscricao, arvore->raiz->proximo);
-    }
-    arvore->elementos--;
-    libertarNoInscricao(inscricao);
-    inscricao= NULL;
-    return _SUCESSO;
-}
-
-//Remover Elementos na Subarvore
-NoINSCRICAO * removerElementoNaSubArvore(NoINSCRICAO * inscricao, NoINSCRICAO * subarvore){
-    if(inscricao ==subarvore){
-        if(subarvore->ramo){ //Verifica se existem ramo central
-            if(subarvore->anterior){
-                subarvore->ramo->anterior = inserirElementoArvore(subarvore->anterior, subarvore->ramo);
-            }
-            if(subarvore->proximo){
-                subarvore->ramo->proximo = inserirElementoArvore(subarvore->proximo, subarvore->ramo);
-            }
-            return subarvore->ramo;
-        } else if(subarvore->proximo){ //Verifica se existe ramo seguinte
-            if(subarvore->anterior){
-                subarvore->proximo->anterior = inserirElementoArvore(subarvore->anterior, subarvore->proximo);
-            }
-            return subarvore->proximo;
-        } else{ //Verifica se existe ramo anterior
-            return subarvore->anterior;
-        }
-    } else{
-        int res = wcscmp(inscricao->elemento->anoLetivo, subarvore->elemento->anoLetivo);
-        if(res ==0)
-            subarvore->ramo = removerElementoNaSubArvore(inscricao,subarvore->ramo);
-        else if(res < 0)
-            subarvore->anterior = removerElementoNaSubArvore(inscricao, subarvore->anterior);
-        else
-            subarvore->proximo = removerElementoNaSubArvore(inscricao, subarvore->proximo);
-    }
-    return subarvore;
-}*/
 
 //Incrementar IDAluno
 //Retorna ID Atual
