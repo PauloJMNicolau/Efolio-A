a44898a4ca8f59af6ce0a279fa2e2a9f98b8de41
Rita Borges <ryta.borgez@gmail.com>
2020-04-18 00:42:09 +0100

    Atualização

--- ui/uialunos.c
+++ ui/uialunos.c
@@ -20,7 +20,7 @@ void novoaluno(SGBD * bd){
     wprintf(L"\npais: ");
     wscanf(L"%d", &pais);
     ALUNO * unidade = criaraluno(numero,nome,pais);
-    adicionaraluno(unidade,bd->ucs,bd->ucs->elementos);
+    adicionaraluno(unidade,bd->alunos,bd->alunos->elementos);
     free(nome);
 }
 
@@ -31,34 +31,36 @@ int removerAluno(int pos, LIST_ALUNO *lista);
 
 //consultarAluno
 void consultaralunos(SGBD * bd){
+    int numero, nome , pais;
     clearScreen();
     for(int i =0; i< 80; i++)
         wprintf(L"-");
     wprintf(L"\n|%8S%50S%10S%10S|\n",L"Numero",L"Nome",L"pais");
     for(int i =0; i< 80; i++)
         wprintf(L"-");
-    for(int i=0; i< bd->ucs->elementos; i++){
+    for(int i=0; i< bd->alunos->elementos; i++){
         UC* temp = obterUC(i, bd->ucs);
         wprintf(L"\n|%8d%50S%10d%10d|",temp->numero, temp->nome, temp->ano, temp->semestre);    wprintf(L"\nNumero: ");
-    wscanf(L"%d", &numero);
-    wprintf(L"\nNome: ");
-    wscanf(L"%s", nome);
-    wprintf(L"\npais: ");
-    wscanf(L"%d", &pais);
-    Aluno * unidade = criaraluno(numero,nome,pais);
-    adicionaraluno(unidade,bd->ucs,bd->ucs->elementos);
-    free(nome);
+        wscanf(L"%d", &numero);
+        wprintf(L"\nNome: ");
+        wscanf(L"%s", nome);
+        wprintf(L"\npais: ");
+        wscanf(L"%d", &pais);
+        ALUNO * unidade = criaraluno(numero,nome,pais);
+        adicionaraluno(unidade,bd->alunos,bd->alunos->elementos);
+        free(nome);
 }
     wprintf(L"\n");
     for(int i =0; i< 80; i++)
         wprintf(L"-");
-    wprintf(L"\n|%70S%8d|\n",L"Total Disciplinas",bd->ucs->elementos);
+    wprintf(L"\n|%70S%8d|\n",L"Total Alunos",bd->alunos->elementos);
     for(int i =0; i< 80; i++)
         wprintf(L"-");
-    wprintf(L"\nPara continuar precione ENTER",bd->ucs->elementos);
+    wprintf(L"\nPara continuar precione ENTER",bd->alunos->elementos);
     wchar_t tecla = L' ';
     getwchar();
     do{
         wscanf(L"%c", &tecla);
     } while(tecla != L'\n');
 }
+//modificarAluno
