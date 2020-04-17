feea472fee0d360294063fff3d098b302790cdca
hcoelho <coelhohernani09@gmail.com>
2020-04-17 22:50:49 +0000

    novaInscricao

--- ui/uiinscricoes.c
+++ ui/uiinscricoes.c
@@ -2,26 +2,33 @@
 
 //Adicionar uma nova Inscrição
 void novaInscricao(SGBD * bd) {
+    clearScreen();
     INSCRICAO * novo;
     int numeroUC, numeroAluno;
-    wchar_t * anoLetivo;    
-    clearScreen();
-    
-    anoLetivo = calloc(_TAMSTRING, sizeof(wchar_t)); //Na macro pode-se definir um size para datas
-
+    wchar_t * anoLetivo, tecla = L' ';   
+    anoLetivo = calloc(_TAMDATAS, sizeof(wchar_t)); 
     wprintf(L"Preencha os dados da nova Inscrição");
     wprintf(L"\nNúmero da UC: ");
     wscanf(L"%d", &numeroUC);
-    wprintf(L"\nNúmero de aluno: ");
-    wscanf(L"%d", &numeroAluno);
-    wprintf(L"\nAno Letivo: ");
-    wscanf(L"%s", anoLetivo);
-
     if (procurarUC(numeroUC, bd->ucs)) {
-        novo = criarInscricao(numeroUC, numeroAluno, anoLetivo);
-        adicionarInscricao(novo, bd->inscricoes);
+        wprintf(L"\nNúmero de aluno: ");
+        wscanf(L"%d", &numeroAluno);
+        if (procurarAluno(numeroAluno, bd->alunos)) {
+            wprintf(L"\nAno Letivo [xxxx/yyyy]: ");
+            wscanf(L"%ls", anoLetivo);
+            novo = criarInscricao(numeroUC, numeroAluno, anoLetivo);
+            adicionarInscricao(novo, bd->inscricoes);
+        }
+        else
+            wprintf(L"Número de aluno inexistente");
     }
     else
-        printf("uc não existe");
-    
+        wprintf(L"Unidade Curricular inexistente");
+    free(anoLetivo);
+    wprintf(L"\nPara continuar pressione ENTER");
+    getwchar();
+    do{
+        wscanf(L"%c", &tecla);
+    } while(tecla != L'\n');
 }
+
