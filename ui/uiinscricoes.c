df97f2570468384459306b00e9a314e2b3c66f51
hcoelho <coelhohernani09@gmail.com>
2020-04-17 22:49:56 +0000

    procurarUC procurarAluno

--- ui/uiinscricoes.c
+++ ui/uiinscricoes.c
@@ -17,7 +17,10 @@ void novaInscricao(SGBD * bd) {
             wprintf(L"\nAno Letivo [xxxx/yyyy]: ");
             wscanf(L"%ls", anoLetivo);
             novo = criarInscricao(numeroUC, numeroAluno, anoLetivo);
-            adicionarInscricao(novo, bd->inscricoes);
+            if(novo != NULL && adicionarInscricao(novo, bd->inscricoes) == _SUCESSO)
+                wprintf(L"Incrição Efectuada");
+            else
+                wprintf(L"Erro na Inscrição");    
         }
         else
             wprintf(L"Número de aluno inexistente");
@@ -32,10 +35,45 @@ void novaInscricao(SGBD * bd) {
     } while(tecla != L'\n');
 }
 
-//Mostrar lista de inscrições
+//Mostrar lista de inscrições por ano letivo
 void mostrarListaInscricoes(SGBD * bd){
     clearScreen();
+    NO_PASTA * pasta;
+    NO * tmp;
+    int i, j = 0;
+    wchar_t * anoLetivo, tecla = L' ';
+    anoLetivo = calloc(_TAMDATAS, sizeof(wchar_t));
+    wprintf(L"\nIndique o ano letivo que pretende consultar: ");
+    wscanf(L"%ls", anoLetivo);
 
-
+    pasta = procuraPasta(anoLetivo, bd->inscricoes);
+    
+    if (pasta != NULL){
+        tmp = pasta->cauda;
+        for(i =0; i<80; i++)
+            wprintf(L"-");
+        wprintf(L"\n|%25S|%25S|%26S|\n",L"Número Aluno",L"Número de UC",L"Ano Letivo");
+        for(i =0; i<80; i++)
+            wprintf(L"-");
+        wprintf(L"\n");
+        while (j < pasta->elementos) {
+            wprintf(L"|%25d|%25d|%26S|\n", tmp->elemento->numeroAluno, tmp->elemento->numeroUC, tmp->elemento->anoLetivo);
+            tmp = tmp->proximo;
+            j++;
+        }
+        for(i =0; i<80; i++)
+            wprintf(L"-");
+        wprintf(L"\n|%52S%26d|\n", L"Total de Inscrições",pasta->elementos);
+        for(i =0; i<80; i++)
+            wprintf(L"-");
+    }
+    else
+        wprintf(L"Não existe inscrições para o ano letivo indicado");
+    free(anoLetivo);
+    wprintf(L"\nPara continuar pressione ENTER");
+    getwchar();
+    do{
+        wscanf(L"%c", &tecla);
+    } while(tecla != L'\n');
 }
 
