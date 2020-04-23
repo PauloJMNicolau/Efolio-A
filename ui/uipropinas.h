c0f55803c70e8356831b4fc670d040ef827bc156
hcoelho <coelhohernani09@gmail.com>
2020-04-23 13:36:08 +0000

    Calculo de propinas

--- ui/uipropinas.h
+++ ui/uipropinas.h
@@ -6,5 +6,8 @@
 
 //Função para calcular valor da propina
 void calcularPropinas(SGBD * bd);
-
+//Cálculo da propina mediante condicão do estudante
+int calculo(int ects, int opcao);
+//Verifica estatuto de residente do estudante e se é 1º inscrição
+int condicaoPropina(ALUNO *aluno,LISTA_PASTA * inscricao);
 #endif
